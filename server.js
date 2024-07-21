3954043a502764e990344170fd59c8c5b875ca25
ohnoitsjarvis <jayeshy2806@gmail.com>
2024-07-20 22:55:58 -0400

    did it finally
    yayayyy

--- server.js
+++ server.js
@@ -1,104 +1,65 @@
 /********************************************************************************
-* WEB322 – Assignment 02
+* WEB322 – Assignment 04
 *
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 *
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Ishika Munjal Student ID: 172502213 Date: 25/06/2023
+* Name: Ishika Munjal Student ID: 172502213 Date: 20/07/2024
 *
-*  Published URL: https://assignment-3-omega-steel.vercel.app/
+*  Published URL: https://assignment-4-omega-steel.vercel.app/
 *
 ********************************************************************************/
 const express = require('express');
-const path = require('path');
+const path = require('path');   
 const legoData = require('./modules/legoSets');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-legoData.initialize()
-    .then(() => {
-        console.log('Lego data initialized successfully.');
-        
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-        app.get('/', (req, res) => {
-            res.send('Assignment 3: Ishika Munjal - 172502213');
-        });
+// Ensure legoData is initialized before starting the server
+legoData.initialize().then(() => {
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}).catch(err => {
+  console.error('Failed to initialize legoData', err);
+});
 
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, '/views/home.html'));
-        });
+app.get('/', (req, res) => {
+  res.render('home', { name: 'Your Name', studentId: 'Your Student ID' });
+});
 
-        app.get('/about', (req, res) => {
-            res.sendFile(path.join(__dirname, '/views/about.html'));
-        });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-        app.get('/lego/sets', (req, res) => {
-            if (req.query.theme) {
-                legoData.getSetsByTheme(req.query.theme)
-                    .then((sets) => {
-                        res.json(sets);
-                    })
-                    .catch((err) => {
-                        res.status(404).send(err);
-                    });
-            } else {
-                legoData.getAllSets()
-                    .then((sets) => {
-                        res.json(sets);
-                    })
-                    .catch((err) => {
-                        res.status(404).send(err);
-                    });
-            }
-        });
+app.get('/lego/sets', (req, res) => {
+  legoData.getAllSets().then((sets) => {
+    res.render('sets', { sets });
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
+});
 
-        app.get('/lego/sets', (req, res) => {
-            legoData.getAllSets()
-                .then(data => res.json(data))
-                .catch(err => res.status(500).send(err));
-        });
+app.get('/lego/sets/num-demo', (req, res) => {
+  legoData.getSetByNum('1234').then((set) => {
+    res.render('set', { set });
+  }).catch((err) => {
+    res.status(404).render('404', { message: err });
+  });
+});
 
-        app.get('/lego/sets/num-demo', (req, res) => {
-            legoData.getSetByNum('001-1')
-                .then(data => res.json(data))
-                .catch(err => res.status(404).send(err));
-        });
+app.get('/lego/sets/theme-demo', (req, res) => {
+  legoData.getSetsByTheme('technic').then((sets) => {
+    res.render('sets', { sets });
+  }).catch((err) => {
+    res.status(404).render('404', { message: err });
+  });
+});
 
-        app.get('/lego/sets/theme-demo', (req, res) => {
-            legoData.getSetsByTheme('tech')
-                .then(data => res.json(data))
-                .catch(err => res.status(404).send(err));
-        });
-        app.use(express.static('public'));
-
-        app.use((req, res, next) => {
-            res.status(404).send("404 - We're unable to find what you're looking for.");
-          });
-          app.get('/lego/sets/:set_num', (req, res) => {
-            legoData.getSetByNum(req.params.set_num)
-                .then((set) => {
-                    res.json(set);
-                })
-                .catch((err) => {
-                    res.status(404).send(err);
-                });
-        });
-
-        app.use((req, res) => {
-            res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
-        });
-
-        app.listen(port, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        });
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error(`Unable to start server: ${err}`);
-    });
+app.use((req, res) => {
+  res.status(404).render('404', { message: 'Page not found' });
+});
